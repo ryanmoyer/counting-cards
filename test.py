ee4e73faf92f468157f4f3f1893a7c79abedebec
Ryan Moyer <rymoyer@gmail.com>
2012-12-24 17:06:47 -0500

    Add flake8 requirements to tests.

--- test.py
+++ test.py
@@ -2,17 +2,28 @@
 
 # Test runner
 
+from __future__ import print_function
 import unittest
+import subprocess
 
 
 def main():
-    """Runs unit test discovery. Equivalent to this on the command-line::
+    """First runs flake8 syntax checker. Equivalent to this on the
+    command line::
+
+        flake8 .
+
+    Then runs unit test discovery. Equivalent to this on the command line::
 
         python -m unittest discover tests
 
     No need to return anything since :func:`unittest.main()` directly calls
     :func:`sys.exit()`.
     """
+    print('Syntax check')
+    subprocess.check_call(['flake8', '.'])
+    print('Syntax OK')
+    print('Unit tests')
     unittest.main(argv=['', 'discover', 'tests'])
 
 if __name__ == '__main__':
